15d316f3bed9b86d12e70743ef9ac9ff4d3c0c09
shruti <shrutimsvaidya@gmail.com>
2024-06-27 18:31:31 +0530

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  return (
+  const [weatherInput, setWeatherInput] = React.useState({
+    temperature: "",
+    conditions: "",
+  });
+  React.useEffect(() => {
+    setWeatherInput({ temperature: 25, conditions: "Sunny" });
+  }, []);
+  return(
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay {...weatherInput} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
